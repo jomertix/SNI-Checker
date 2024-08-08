12d9aa8c13a778f9555e1db412372ecec21c26c5
jomertix <jomjobo@proton.me>
2024-08-08 13:15:02 +0300

    Fix typo

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 sudo apt update
 sudo apt install git pip -y
-git clone https://github.com/jomertix/SNI-checker.git
+git clone https://github.com/jomertix/SNI-Checker.git
 cd SNI-Checker
 pip install -r requirements.txt
