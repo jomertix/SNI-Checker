c16de533b2934edfae0e5d3ccf4a2b630925a6d9
jomertix <150632538+jomertix@users.noreply.github.com>
2024-08-09 17:42:39 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt install git pip -y
 git clone https://github.com/jomertix/SNI-Checker.git
-(cd SNI-Checker && pip install -r requirements.txt)
+(cd SNI-Checker && pip install -r requirements.txt -q --exists-action i)
